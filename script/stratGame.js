d2b25ea0ee24ce04ad462d9068dc8f06f9e865f6
chentaymane <chentaymane234@gmail.com>
2026-06-03 17:12:06 +0100

    add paddle and breaks

--- script/stratGame.js
+++ script/stratGame.js
@@ -4,16 +4,48 @@ let pauseBtn = document.getElementById('pause-btn');
 let pauseOverlay = document.getElementById('pause-overlay');
 let menuContainer = document.getElementById('menu-container');
 let startBtn = document.querySelector('.start-button');
-
+let bricksContainer = document.querySelector('.bricks-container');
+let paddle = document.createElement("div");
+   
 startBtn.addEventListener('click', function() {
     menuContainer.style.display = 'none';
     pauseBtn.hidden = false;
+
+     paddle.id = "paddle";
+    document.body.appendChild(paddle);
+
+    // create 2 rows
+    for (let row = 0; row < 5; row++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.add("brick-row");
+
+        for (let i = 0; i <6; i++) {
+            let brick = document.createElement("div");
+            brick.classList.add("brick");
+
+            // color logic
+            if (row % 2=== 0) {
+                brick.classList.add("red");
+            } else {
+                brick.classList.add("yellow");
+            }
+
+            rowDiv.appendChild(brick);
+        }
+
+        bricksContainer.appendChild(rowDiv);
+    }
+
 });
 
-// 2. PAUSE BUTTON: Show the overlay
+document.addEventListener("mousemove", (e) => {
+    let x = e.clientX;
+
+    paddle.style.left = x + "px";
+});
+
 pauseBtn.addEventListener('click', function() {
     pauseOverlay.classList.remove('hidden');
-    // logic to stop your game movement goes here (isPaused = true)
 });
 
 resumeBtn.addEventListener('click', function() {
@@ -25,3 +57,6 @@ restartBtn.addEventListener('click', function() {
     console.log('Restart');
     location.reload();
 });
+
+
+
